4d01d0e91702e09c69cefdaa6d200bba7e5c8854
megan <msmeganmcmanus@gmail.com>
2020-08-08 16:57:15 -0700

    small modification to notes

--- colorchange.js
+++ colorchange.js
@@ -1,8 +1,8 @@
-document.addEventListener('mousemove', function (e) {
-    // console.log(e.pageX, e.pageY);
-    const r = Math.round(e.pageX * 255 / window.innerWidth);
-    const b = Math.round(e.pageY * 255 / window.innerHeight);
-    const g = r / b;  // can do many things here
-    const color = `rgb(${r}, ${g}, ${b})`
-    document.body.style.backgroundColor = color;
+document.addEventListener('mousemove', function(e) {
+	// console.log(e.pageX, e.pageY);
+	const r = Math.round(e.pageX * 255 / window.innerWidth);
+	const b = Math.round(e.pageY * 255 / window.innerHeight);
+	const g = r / b; // can do many things here
+	//const g = Math.round(e.pageY * 255 / window.innerHeight);
+	document.body.style.backgroundColor = color;
 });
